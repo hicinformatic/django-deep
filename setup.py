64e4703af1426e5ce3139a70bf918f87d12f539e
Delencre Charles <hicinformatic@gmail.com>
2025-07-04 17:22:48 +0200

    init v0

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
+    author="hicinformatic",
+    author_email="hicinformatic@gmail.com",
     name="django-deep",  # Le nom du package
     version="0.1",  # La version du package
     packages=find_packages(),  # Détecte tous les modules dans django_deep/
@@ -9,15 +14,14 @@ setup(
     install_requires=[  # Les dépendances nécessaires
         "Django>=3.2",  # Exemple: dépendance à une version de Django
     ],
-    author="Ton Nom",
-    author_email="ton.email@example.com",
     description="Django Enhanced Expression Parser for SQL filtering.",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
+    license="GNU General Public License v3 (GPLv3)",
     classifiers=[
         "Framework :: Django",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
-    url="https://github.com/toncompte/django-deep",  # Lien vers ton repo GitHub
+    url="https://github.com/hicinformatic/django-deep",  # Lien vers ton repo GitHub
 )
